6c1dc36d5281037efd37516469592071d6a67bfa
adrputra <56150878+adrputra@users.noreply.github.com>
2022-05-18 14:09:34 +0700

    Add files via upload

---
+++ koneksi.php
@@ -0,0 +1,8 @@
+
+<?php
+    $host=" sql313.epizy.com ";
+    $user="epiz_30648573";
+    $pwd="1ejh6a84ze";
+    $dbname="epiz_30648573_sikimdas";
+    $kon=mysqli_connect($host,$user,$pwd,$dbname);
+?>
